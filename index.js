5915d1235c9990c4c6afa577c504f3c505b5e27c
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-01-04 02:03:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+const { user } = require('./db')
+const { admin } = require('./db')
+const { course } = require('./db')
+const { purchase } = require('./db')
+const { courseRouter } = require('./routes/course')
+const { adminRouter } = require('./routes/admin')
+const { userRouter } = require('./routes/user')
+
+const app = express();
+
+app.use("/user", userRouter)
+app.use("/course", courseRouter)
+app.use("/admin", adminRouter)
+
+async function main(){
+    await mongoose.connect(process.env.MONGO_URL);
+    app.listen(3000);
+    console.log("Server running at port 3000");
+}
+
+main();
---
+++ routes/admin.js
@@ -0,0 +1,29 @@
+const { Router } = require('express');
+const { admin } = require('../db');
+const adminRouter = Router();
+
+
+adminRouter.post('/signup', (req, res) => {
+
+});
+
+adminRouter.post('/signup', (req, res) => {
+
+});
+
+adminRouter.post('/course', (req, res) => {
+
+});
+
+adminRouter.put('/course', (req, res) => {
+
+});
+
+adminRouter.get('/course/bulk', (req, res) => {
+
+});
+
+
+module.exports = {
+    adminRouter: adminRouter
+}
