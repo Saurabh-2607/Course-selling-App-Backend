6f3a6d403aac9da671110e57a3bd08db07be513d
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-01-05 22:19:53 +0530

    implemented Course endpoints

--- routes/course.js
+++ routes/course.js
@@ -3,12 +3,15 @@ const courseRouter = Router();
 
 
 courseRouter.post('/purchase', (req, res) => {
-
-});
-
+    const course = req.body.course;
+    const user = req.body.user;
+    res.json({ success: true });
+}
+);
 
 courseRouter.get('/preview', (req, res) => {
-
+    const course = req.query.course;
+    res.json({ success: true });
 });
 
 module.exports = {
