00481cd0a4b25d3239acbe7eb5e1e96219e46134
Saurabh-2607 <saurabh.nayla@gmail.com>
2025-01-05 15:39:19 +0530

    Implemeted User End points

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const bcrypt = require('bcrypt');
+const {userMiddleware} = require('../middlewares/user');
 const jwt = require('jsonwebtoken');
 const { user } = require('../db');
+const { purchase } = require('../db');
 const userRouter = Router();
 
 userRouter.post('/signup',async (req, res) => {
@@ -24,17 +26,48 @@ userRouter.post('/signup',async (req, res) => {
                 lastname: lastname
             });
             res.status(200).json({
-                message: 'Admin created successfully'
+                message: 'user created successfully'
             });        
     }
 });
 
-userRouter.post('/signin', (req, res) => {
+userRouter.post('/signin',async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const search = await user.findOne({email: email});
+    if(search){
+        if(await bcrypt.compare(password, search.password)){
+            res.status(200).json({
+                token: jwt.sign({email: email}, process.env.USER_SECRET),
+                message: 'Login successful'
+            });
+        }
+        else{
+            res.status(400).json({
+                message: 'incorrect password'
+            });
+        }
+    }
+    else{
+        res.status(400).json({
+            message: 'Email not found please signup'
+        });
+    }
 });
 
-userRouter.get('/purchases', (req, res) => {
+userRouter.get('/purchases',async (req, res) => {
+    const email = jwt.decode(req.headers.authorization, process.env.USER_SECRET);
+    const userDoc = await user.findOne({ email: email.email });
+    const userID = userDoc._id;
 
+    const courses = await purchase.find(
+        {userId: userID}
+    )
+
+    res.send(
+        courses
+    );
 });
 
 module.exports = {
